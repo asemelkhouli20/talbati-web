62dc48c5da7e99010d13597c518875a75d7fa276
asemElkhouli <asemelkhouli209@gmail.com>
2025-03-28 03:34:10 +0200

    blank

--- assets/js/device.js
+++ assets/js/device.js
@@ -3,9 +3,12 @@ function redirectToStore() {
   if (/android/i.test(userAgent)) {
     // Redirect to Google Play Store
     // window.location.href = "https://play.google.com/store/apps/details?id=com.talbat.app";
+    window.open("https://api.talabati.plus/ar", "_blank");
   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     // Redirect to Apple App Store
     window.location.href = "https://apps.apple.com/app/id1596505868";
+  }else{
+    window.open("https://api.talabati.plus/ar", "_blank");
   }
 }
 // Call the function when needed
